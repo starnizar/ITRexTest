cef8f6a1975011eb1d6fb02a78a25ac86203286f
starnizar <nizarincorp@gmail.com>
2021-06-08 19:04:52 +0300

    Start

---
+++ src/components/FirstTask/FirstTask.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FirstTask = () => {
+    const getCurrencies = async() => {
+        const response = await fetch(``)
+        const result = await response.json
+    }
+    return <div className='taskBox'>
+
+    </div>
+}   
+
+export default FirstTask
